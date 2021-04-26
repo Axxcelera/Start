9e32752e76ab1c3bfb70e377287bde446ae1b4a4
Ayush <80700437+Axxcelera@users.noreply.github.com>
2021-04-26 22:19:30 +0530

    Create 7.Celcius to fahrenheit.py

---
+++ 7.Celcius to fahrenheit.py
@@ -0,0 +1,7 @@
+#Program to convert temperature from celcius to fahrenheit
+
+celcius = float(input("Enter Temperature in Celcius: "))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print(f'Temperature in fahrenheit is : {fahrenheit}')
