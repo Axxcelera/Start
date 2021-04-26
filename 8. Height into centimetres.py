5276ebb60d37e945306337ca9531da803119bc0d
Ayush <80700437+Axxcelera@users.noreply.github.com>
2021-04-26 22:25:28 +0530

    Create 8. Height into centimetres.py

---
+++ 8. Height into centimetres.py
@@ -0,0 +1,8 @@
+#Program to calculate height into centimetres
+
+foot = float(input("Enter Height in Foot : "))
+inch = 2.54
+
+centimetres =  foot * 12 * inch
+
+print(f'Height into centimeters is : {centimetres}')
