f8f4fd81b7e36da649062e3129d38d7c632eecc4
Ayush <80700437+Axxcelera@users.noreply.github.com>
2021-04-27 15:01:33 +0530

    Create vowel_or_consonant.py

---
+++ vowel_or_consonant.py
@@ -0,0 +1,7 @@
+#Python program to check whether the entered letter is vowel or consonant.
+
+letter = input("Enter the Letter : ")
+if letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u' or letter=='A' or letter=='E' or letter=='I' or letter=='O' or letter=='U' :
+    print(f"Entered letter {letter} is a vowel.")
+else:
+    print(f"Entered letter {letter} is a consonant.")
