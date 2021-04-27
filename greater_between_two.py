c5a7f06bca6d04b1eb4627c36bd3df8c8c3d2695
Ayush <80700437+Axxcelera@users.noreply.github.com>
2021-04-27 12:44:33 +0530

    Create greater_between_two.py

---
+++ greater_between_two.py
@@ -0,0 +1,10 @@
+#Pyhton program to check greater between two numbers.
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+if num1>num2:
+    print(f"First number {num1} is greater than second number {num2}")
+else:
+    print(f"Second number {num2} is greater than first number {num1}")
+    
