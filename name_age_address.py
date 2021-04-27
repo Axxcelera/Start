6979e01c59bc7965ca6a6c8fb1a5ea9b0dd02b66
Ayush <80700437+Axxcelera@users.noreply.github.com>
2021-04-27 12:21:01 +0530

    Create name_age_address.py

---
+++ name_age_address.py
@@ -0,0 +1,7 @@
+#Python program to display name age address of different person.
+
+name = input("Enter the name of the person : ")
+age = int(input("Enter age of the person : "))
+add = input("Enter address of the person : ")
+
+print(f'Name : {name} \nAge : {age} \nAddress : {add} ')
